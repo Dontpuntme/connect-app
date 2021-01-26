f3f47152cb331a88e7a6c9aaa3e4e7530609fbb4
Dontpuntme <david.martindale.99@gmail.com>
2021-01-26 16:53:45 -0500

    Got rid of old stuff and Put profile on the same Line

--- connect-app/pages/index.js
+++ connect-app/pages/index.js
@@ -30,7 +30,7 @@ function ProfileAvatar() {
   const classes = useStyles();
   return (
     <div>
-    <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" className = {classes.large} />
+    <Avatar style= {{ width: '200px', height: '200px'}}alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
     </div>
   );
 }
@@ -115,47 +115,16 @@ export default function Home() {
 
       
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-        <ProfileAvatar></ProfileAvatar>
-        
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <div style= {{ width: '600px', height: '100px'}}>
+          <div style= {{ width: '50px', float: 'left'}} >
+          <ProfileAvatar></ProfileAvatar>
+          </div>
+          <div style= {{ width: '300px', float: 'right'}} ><p>Profile Name</p>
+          </div>
+          </div>
+          <div></div>
+          <header>Bio</header>
       </main>
-
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
